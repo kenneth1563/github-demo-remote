cc11be8067ec53610f56e1713706b33dd23265c5
kenneth <kenneth_thomson@persistent.com>
2021-11-25 09:00:49 -0800

    add function headers

--- math.py
+++ math.py
@@ -1,13 +1,17 @@
 # math.py for GIT TUTORIAL
 
+#add implementation
 def add(x,y):
     pass
 
+#subtract implementation
 def subtract(x,y):
     pass
 
+#multiply implementation
 def multiply(x,y):
     pass
 
+#divide implementation
 def divide(x,y):
     pass
